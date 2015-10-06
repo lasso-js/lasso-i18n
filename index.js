cc63d8ad04422ab24b5121a56a9106ca1b503cdf
Phillip Gates-Idem <phillip.idem@gmail.com>
2015-10-06 13:01:11 -0400

    Upgrade Marko and allow server-side compilation of dictionaries

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
 var util = require('./util');
+var nodePath = require('path');
+var markoLoader = require('marko/runtime/loader');
+var Dictionary = require('./Dictionary');
 
-module.exports = {
-    normalizeLocaleCode: util.normalizeLocaleCode,
+var types = {
+    'marko': function(context) {
+        var value = context.value;
 
-    /**
-     * The method will return the most appropriate locale based on the
-     * end-user's locale preference and available locales. This method will
-     * handle both the "_" and "-" separator characters equally well.
-     *
-     * @param preferredLocale an array of locales (e.g. ['en', 'en_US'])
-     *  or a string in the format of the Accept-Language header value
-     *  (e.g. "en-US,en;q=0.8")
-     *
-     * @param availableLocales an array of locale codes
-     *  (e.g. ["en", "en_US", ])
-     *
-     * @return the item in the availableLocales that is a best match or
-     *  the empty string to indicate default locale
-     */
-    findBestLocale: util.findBestLocale
+        if (Array.isArray(value)) {
+            value = value.join('\n');
+        }
+
+        var dirname = nodePath.dirname(context.name);
+        var templatePath = nodePath.join(dirname, context.key + '.marko');
+
+        var template = markoLoader(templatePath, value);
+
+        return function(data) {
+            return template.renderSync(data);
+        };
+    }
+};
+
+exports.normalizeLocaleCode = util.normalizeLocaleCode;
+
+/**
+ * The method will return the most appropriate locale based on the
+ * end-user's locale preference and available locales. This method will
+ * handle both the "_" and "-" separator characters equally well.
+ *
+ * @param preferredLocale an array of locales (e.g. ['en', 'en_US'])
+ *  or a string in the format of the Accept-Language header value
+ *  (e.g. "en-US,en;q=0.8")
+ *
+ * @param availableLocales an array of locale codes
+ *  (e.g. ["en", "en_US", ])
+ *
+ * @return the item in the availableLocales that is a best match or
+ *  the empty string to indicate default locale
+ */
+exports.findBestLocale = util.findBestLocale;
+
+exports.compileDictionary = function(obj, name, localeCode) {
+    var keys = Object.keys(obj);
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        var value = obj[key];
+        var compiler = null;
+
+        var pos = key.indexOf(':');
+        if (pos !== -1) {
+            var type = key.substring(0, pos).trim().toLowerCase();
+            compiler = types[type];
+            if (compiler) {
+                delete obj[key];
+                key = key.substring(pos + 1).trim();
+            }
+        }
+
+        if (compiler) {
+            obj[key] = compiler({
+                value: value,
+                key: key,
+                name: name
+            });
+        }
+    }
+
+    return new Dictionary(name, obj, localeCode);
 };
